b563e3a3181d8c44a946033afe53072fcc524c49
Arcana-Mace <flaxxxx@mail.ru>
2020-12-08 19:05:29 +0300

    models

--- news/models.py
+++ news/models.py
@@ -11,6 +11,6 @@ class Articles(models.Model):
     def __str__(self):
         return self.title
 
-    class Meta():
+    class Meta:
         verbose_name='Новость'
         verbose_name_plural='Новости'
